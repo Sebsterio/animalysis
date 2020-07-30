ed5f99812f660db22354f4442865100dafa28dda
Sebsterio <sebastian.rosloniec@gmail.com>
2020-07-30 19:44:37 +0200

    set up back end and connect to heroku

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -2,5 +2,9 @@ import React from "react";
 import "./App.scss";
 
 export const App = () => {
-	return <div className="App">App</div>;
+	return (
+		<div className="App">
+			<h1>App under construction</h1>
+		</div>
+	);
 };
---
+++ server/app.js
@@ -0,0 +1,64 @@
+const express = require("express");
+// const mongoose = require("mongoose");
+const path = require("path");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const compression = require("compression");
+
+dotenv.config();
+
+const app = express();
+
+// -------------- Middleware -------------
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+if (process.env.NODE_ENV !== "production") {
+	const morgan = require("morgan");
+	app.use(morgan("dev"));
+}
+
+if (process.env.NODE_ENV === "production") {
+	app.use(compression());
+}
+
+// ---------------- Mongo ----------------
+
+// const db = process.env.MONGO_URI;
+
+// mongoose
+// 	.connect(db, {
+// 		useNewUrlParser: true,
+// 		useCreateIndex: true,
+// 		useUnifiedTopology: true,
+// 		useFindAndModify: false,
+// 	})
+// 	.then(() => console.log("MongoDB Connected..."))
+// 	.catch((err) => console.log(err));
+
+// --------------- Routing ---------------
+
+// app.use("/api/auth", authRoutes);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "..", "client", "build")));
+	app.get("*", (req, res) => {
+		res.sendFile(
+			path.resolve(__dirname, "..", "client", "build", "index.html")
+		);
+	});
+}
+
+// ---------------------------------------
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, (err) => {
+	if (err) throw err;
+	console.log(`Server started on PORT ${port}`);
+});
+
+module.exports = app;
