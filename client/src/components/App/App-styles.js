f961383dfc98a9650e0bfd6525f571286c77a0e1
Sebsterio <sebastian.rosloniec@gmail.com>
2020-08-03 19:59:40 +0200

    useStyles in App

---
+++ client/src/components/App/App-styles.js
@@ -0,0 +1,25 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+	app: {
+		position: "absolute",
+		width: "100vw",
+		height: "100vh",
+		display: "flex",
+		flexFlow: "column nowrap",
+		justifyContent: "stretch",
+		alignItems: "stretch",
+	},
+	header: {
+		position: "relative",
+		flex: "0 0 auto",
+	},
+	main: {
+		position: "relative",
+		flex: "1 1 0",
+		width: "100vw",
+		display: "flex",
+		justifyContent: "center",
+		alignItems: "stretch",
+	},
+}));
--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -5,22 +5,24 @@ import { Header } from "components/Header";
 import { Account } from "pages/Account";
 
 import { routes } from "routes";
-import "./App.scss";
+import { useStyles } from "./App-styles";
 
 /*******************************
+ * App layout
  * Root-level Routing
  * Trigger authentication (TODO)
- * Main layout & semantic HTML tags
  *******************************/
 
 export const App = ({ isAuthenticated }) => {
+	const clx = useStyles();
+
 	// Create Routes from routes array
 	const mainRoutes = routes.map((route) => {
 		const { path, component, exact } = route;
 		return <Route exact={exact} path={path} component={component} key={path} />;
 	});
 
-	// Redirect to /account/sing-in if user not authenticated
+	// Redirect to '/account/sing-in' if user not authenticated
 	const authRoutes = (
 		<>
 			<Route path="/account/:mode" component={Account} />
@@ -29,13 +31,13 @@ export const App = ({ isAuthenticated }) => {
 	);
 
 	return (
-		<div className="App">
+		<div className={clx.app}>
 			{isAuthenticated && (
-				<header className="App__header">
+				<header className={clx.header}>
 					<Header />
 				</header>
 			)}
-			<main className="App__main">
+			<main className={clx.main}>
 				<Switch>{isAuthenticated ? mainRoutes : authRoutes}</Switch>
 			</main>
 		</div>
