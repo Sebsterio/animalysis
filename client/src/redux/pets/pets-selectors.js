01c31e72bc64b5bfd29ff9d31ba14534716bd36a
Sebsterio <sebastian.rosloniec@gmail.com>
2020-08-17 21:42:51 +0200

    minor bug fix

--- client/src/redux/pets/pets-selectors.js
+++ client/src/redux/pets/pets-selectors.js
@@ -10,7 +10,7 @@ export const getPetByName = (state, name) =>
 		(pet) => pet.name.toLowerCase() === name.toLowerCase()
 	);
 
-export const getPetReports = (pet) => pet.reports;
+export const getPetReports = (pet) => pet.reports || [];
 
 // -------------- reports ----------------
 
