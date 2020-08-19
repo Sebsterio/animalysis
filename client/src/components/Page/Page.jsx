56295ba51e3cc30bc368f96b64d1fa7713e0625c
Sebsterio <sebastian.rosloniec@gmail.com>
2020-08-19 20:56:18 +0200

    tweak Page

--- client/src/components/Page/Page.jsx
+++ client/src/components/Page/Page.jsx
@@ -3,41 +3,46 @@ import { makeStyles } from "@material-ui/core/styles";
 import { Container } from "@material-ui/core";
 
 /*************************************************
- * Grows vertically to contain children, limited height
+ * Container: assummed to be stretched by parent (x & y)
+ * Page: Grows vertically to contain children, limited height
  * Header & footer: optional, non-scrollable
- * Content container: Grows vertically to fill free space,
+ * Main: Grows vertically to fill free space,
  *   overflow Y-scrollable
  *************************************************/
 
 const useStyles = makeStyles((theme) => ({
 	page: {
-		maxHeight: "90vh",
+		position: "absolute",
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0,
 		display: "flex",
 		flexFlow: "column nowrap",
 		justifyContent: "space-between",
 		padding: theme.spacing(3),
 	},
-	header: {
-		marginBottom: theme.spacing(4),
-	},
+	header: {},
 	main: {
 		overflowY: "auto",
 		display: "flex",
 		flexFlow: "column nowrap",
 		justifyContent: "center",
+		paddingTop: theme.spacing(4),
+		paddingBottom: theme.spacing(4),
 	},
-	footer: {
-		marginTop: theme.spacing(4),
-	},
+	footer: {},
 }));
 
 export const Page = ({ header, footer, main, children = main }) => {
 	const clx = useStyles();
 	return (
-		<Container maxWidth="xs" className={clx.page}>
-			{header && <div className={clx.header}>{header}</div>}
-			<div className={clx.main}>{children}</div>
-			{footer && <div className={clx.footer}>{footer}</div>}
-		</Container>
+		<div>
+			<Container maxWidth="xs" className={clx.page}>
+				{header && <div className={clx.header}>{header}</div>}
+				<div className={clx.main}>{children}</div>
+				{footer && <div className={clx.footer}>{footer}</div>}
+			</Container>
+		</div>
 	);
 };
