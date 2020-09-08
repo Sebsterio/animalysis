c9695e8da6937c8efd3ed3d480e41d7393b69441
Sebsterio <sebastian.rosloniec@gmail.com>
2020-09-08 21:25:28 +0200

    use Page component in Report

--- client/src/pages/Report/Report.jsx
+++ client/src/pages/Report/Report.jsx
@@ -1,24 +1,13 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 import { makeStyles } from "@material-ui/core/styles";
-import { Container, Typography, Button, Dialog } from "@material-ui/core";
-import { Alert } from "components";
+import { Typography, Button, Dialog } from "@material-ui/core";
+import { Page, Alert } from "components";
 import { Head, ProblemList } from "./components";
 import { getDateString } from "utils/date";
 import { summaryData } from "pages/Summary/Summary-data";
 
 const useStyles = makeStyles((theme) => ({
-	page: {
-		display: "flex",
-		flexFlow: "column nowrap",
-		justifyContent: "space-between",
-		padding: theme.spacing(2),
-	},
-	main: {
-		margin: theme.spacing(4, 0, 0),
-		display: "grid",
-		gridGap: theme.spacing(4),
-	},
 	dialogContent: { padding: theme.spacing(3) },
 }));
 
@@ -45,50 +34,53 @@ const Report = ({ history, match, getReport, getPet, isVet }) => {
 	const sentStatus = sending ? "Sending" : sent ? "Sent" : "Not sent";
 
 	return (
-		<Container maxWidth="xs" className={c.page}>
-			<Head pet={pet} />
+		<>
+			<Page
+				header={<Head pet={pet} />}
+				main={
+					<>
+						<div className={c.titleBlock}>
+							<Typography
+								component="h2"
+								variant="h4"
+								align="center"
+								children={title}
+							/>
+							<Typography
+								component="h3"
+								variant="body1"
+								color="textSecondary"
+								align="center"
+								children={getDateString(dateCreated)}
+							/>
+							<Typography
+								component="h3"
+								variant="body1"
+								color="textSecondary"
+								align="center"
+								children={sentStatus}
+							/>
+						</div>
+						<Alert level={alert} alignLeft clickHandler={openDialog} />
 
-			<div className={c.main}>
-				<div className={c.titleBlock}>
-					<Typography
-						component="h2"
-						variant="h4"
-						align="center"
-						children={title}
+						{!!problemList.length && <ProblemList data={problemList} />}
+					</>
+				}
+				footer={
+					<Button
+						fullWidth
+						variant="outlined"
+						color="default"
+						children="Close"
+						onClick={closePage}
 					/>
-					<Typography
-						component="h3"
-						variant="body1"
-						color="textSecondary"
-						align="center"
-						children={getDateString(dateCreated)}
-					/>
-					<Typography
-						component="h3"
-						variant="body1"
-						color="textSecondary"
-						align="center"
-						children={sentStatus}
-					/>
-				</div>
-
-				<Alert level={alert} alignLeft clickHandler={openDialog} />
-
-				<Dialog open={dialogOpen} onClose={closeDialog} maxWidth="xs">
-					<Typography className={c.dialogContent} children={dialogText} />
-				</Dialog>
-
-				<ProblemList data={problemList} />
-			</div>
-
-			<Button
-				fullWidth
-				variant="outlined"
-				color="default"
-				children="Close"
-				onClick={closePage}
+				}
 			/>
-		</Container>
+
+			<Dialog open={dialogOpen} onClose={closeDialog} maxWidth="xs">
+				<Typography className={c.dialogContent} children={dialogText} />
+			</Dialog>
+		</>
 	);
 };
 
