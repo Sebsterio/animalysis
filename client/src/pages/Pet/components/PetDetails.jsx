9fd84828e534717be333b7ff2f1bba62478c4923
Sebsterio <sebastian.rosloniec@gmail.com>
2020-09-11 19:58:14 +0200

    show owners name on Pet

--- client/src/pages/Pet/components/PetDetails.jsx
+++ client/src/pages/Pet/components/PetDetails.jsx
@@ -5,8 +5,9 @@ import Card from "@material-ui/core/Card";
 import CardContent from "@material-ui/core/CardContent";
 import { Dictionary } from "components";
 
-export const PetDetails = ({ weight, microchip }) => {
-	if (!weight && !microchip) return null;
+export const PetDetails = ({ weight, microchip, owner }) => {
+	const { firstName, surname } = owner;
+	const ownerName = firstName + (surname ? ` ${surname}` : "");
 	return (
 		<Card>
 			<CardContent>
@@ -27,6 +28,10 @@ export const PetDetails = ({ weight, microchip }) => {
 							<Typography>{microchip}</Typography>
 						</>
 					)}
+					<Typography variant="subtitle2" color="primary">
+						Owner:
+					</Typography>
+					<Typography>{ownerName}</Typography>
 				</Dictionary>
 			</CardContent>
 		</Card>
